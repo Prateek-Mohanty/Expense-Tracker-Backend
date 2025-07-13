961a07f514a159287c5389d4098884bb646adf8a
Prateek Mohanty <mohantyprateekkumar@gmail.com>
2025-07-13 15:47:41 +0530

    RestAPI created

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Path
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
-import models, schemas
+import models
+from models import Expense
+from typing import Annotated
+from starlette import status
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -14,19 +18,50 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/expenses", response_model=list[schemas.Expense])
-def read_expenses(db: Session = Depends(get_db)):
-    try:
-        return db.query(models.Expense).all()
-    except Exception as e:
-        raise HTTPException(status_code=500,detail=f"Failed to fetch from database: {str(e)}")
+db_dependecny = Annotated[Session,Depends(get_db)]
 
-@app.post("/create_expense")
-def create_expense(expense: schemas.ExpenseCreate, db: Session = Depends(get_db)):
-    try:
-        db_expense = models.Expense(**expense.model_dump())
-        db.add(db_expense)
-        db.commit()
-        return {"success": True}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Failed to create expense: {str(e)}")
+class ExpenseRequest(BaseModel):
+    expense_name:str = Field(min_length=3, max_lenth=50)
+    amount:float = Field(gt=0)
+    date:str = Field(min_length=10)
+
+
+@app.get("/expenses",status_code=status.HTTP_200_OK)
+async def read_expenses(db: db_dependecny):
+    return db.query(Expense).all()
+
+@app.get('/expense/{expense_id}',status_code=status.HTTP_200_OK)
+async def get_expense_by_id(db:db_dependecny,expense_id:int = Path(gt=0)):
+    expense_model = db.query(Expense).filter(Expense.id == expense_id).first()
+    if expense_model is not None:
+        return expense_model
+    raise HTTPException(status_code=404,detail="Expense not found.")
+
+@app.post('/expenses/create_expense')
+async def create_new_expense(db:db_dependecny,expenserequest:ExpenseRequest):
+    expense_model = Expense(**expenserequest.model_dump())
+
+    db.add(expense_model)
+    db.commit()
+
+@app.put('/expenses/{expense_id}')
+async def update_expense_by_id(db:db_dependecny,expense_request:ExpenseRequest,expense_id:int = Path(gt=0)):
+    expense_model = db.query(Expense).filter(Expense.id == expense_id).first()
+    if expense_model is None:
+        raise HTTPException(status_code=404,detail="Expense not found.")
+    
+    expense_model.amount = expense_request.amount
+    expense_model.expense_name = expense_request.expense_name
+    expense_model.date = expense_request.date
+
+    db.add(expense_model)
+    db.commit()
+
+@app.delete('/expenses/{expense_id}',status_code=status.HTTP_204_NO_CONTENT)
+async def delete_expense_by_id(db:db_dependecny,expense_id:int = Path(gt=0)):
+    expense_model = db.query(Expense).filter(Expense.id == expense_id)
+    if expense_model is None:
+        raise HTTPException(status_code=404,detail="Expense not found.")
+    
+    db.query(Expense).filter(Expense.id == expense_id).delete()
+    db.commit()
